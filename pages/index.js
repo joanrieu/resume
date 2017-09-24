958752968259f24bd26b6b6df9c20f720e85228c
Joan Rieu <joan.rieu@gmail.com>
2017-09-24 20:33:10 +0000

    improve design (layout, bg) and code

--- pages/index.js
+++ pages/index.js
@@ -19,72 +19,108 @@ const FluidTypography = () => (
     `}</style>
 )
 
-const MenuLink = ({href, children}) => (
-    <li className="pure-menu-item">
-        <a href={href}
-            className="pure-menu-link">
-            {children}
-        </a>
-    </li>
-)
-
-export default () => (
-    <div className="root">
-        <Head>
-            <meta name="viewport"
-                content="width=device-width,initial-scale=1" />
-            <link rel="stylesheet"
-                href="https://unpkg.com/purecss@1.0.0/build/pure-min.css"
-                integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w"
-                crossorigin="anonymous" />
-            <link rel="stylesheet"
-                href="https://fonts.googleapis.com/css?family=Saira" />
-            <FluidTypography />
-        </Head>
-        <style>{`
-            body {
-                color: #555;
-                font-family: Saira, sans-serif;
-            }
-            .links {
-                font-size: .8em;
-                text-align: center;
-                border-bottom: 1px solid #ccc;
-            }
+const Section = () => (
+    <section>
+        <style jsx>{`
             section {
                 padding: 5vw;
                 max-width: 30rem;
                 margin: auto;
+                color: white;
+                text-shadow: 0 0 .2em #555;
             }
-            @media (min-width: 40em) {
-                section {
-                    border-left: .4rem solid #ccc;
-                    border-radius: 0 0 0 100%;
+            @media (min-width: 40rem) {
+                section::before {
+                    content: "";
+                    float: left;
+                    height: 20rem;
+                    border-left: .2rem solid white;
+                    transform: rotateZ(-5deg);
                 }
             }
             .hero {
                 font-size: 1.5em;
             }
-            blockquote {
+            h1 {
+                font-family: "Saira", sans-serif;
             }
-            .resume {
+            blockquote {
+                font-style: italic;
+            }
+        `}</style>
+        <div className="hero">
+            <h1>Joan Rieu</h1>
+            <p>Software engineering<br/>& systems architecture</p>
+        </div>
+        <blockquote>I build complex software systems.</blockquote>
+    </section>
+)
+
+const Links = () => (
+    <div className="links pure-menu pure-menu-horizontal pure-menu-scrollable">
+        <style jsx>{`
+            .links {
+                font-size: .8em;
+                text-align: center;
+                position: absolute;
+                bottom: 1rem;
+            }
+            .pure-menu-item {
+                background: rgba(255, 255, 255, .05);
+            }
+            .pure-menu-item:first-child {
+                font-weight: bold;
+            }
+        `}</style>
+        <ul className="pure-menu-list">
+            {[
+                ["Resume (PDF)", "/static/Joan Rieu - Resume.pdf"],
+                ["LinkedIn", "https://www.linkedin.com/in/joanrieu/"],
+                ["GitHub", "https://github.com/joanrieu"],
+                ["Twitter", "https://twitter.com/joanrieu"]
+            ].map(([title, href]) => (
+                <li className="pure-menu-item">
+                    <a href={href}
+                        className="pure-menu-link">
+                        {title}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    </div>
+)
+
+export default () => (
+    <div>
+        <Head>
+            <meta name="viewport"
+                content="width=device-width,initial-scale=1" />
+            <link rel="stylesheet"
+                href="https://unpkg.com/purecss@1.0.0/build/base-min.css"
+                crossorigin="anonymous" />
+            <link rel="stylesheet"
+                href="https://unpkg.com/purecss@1.0.0/build/menus-min.css"
+                crossorigin="anonymous" />
+            <link rel="stylesheet"
+                href="https://fonts.googleapis.com/css?family=Saira|Saira+Semi+Condensed" />
+            <FluidTypography />
+        </Head>
+        <style>{`
+            html,
+            body {
+                height: 100%;
+            }
+            body {
+                /* Photo by Azhar J on Unsplash */
+                background: url(/static/azhar-j-177284.jpg);
+                background-size: cover;
+                background-position: center;
+                background-repeat: no-repeat;
+                font-family: "Saira Semi Condensed", sans-serif;
                 text-align: center;
             }
         `}</style>
-        <div className="links pure-menu pure-menu-horizontal pure-menu-scrollable">
-            <ul className="pure-menu-list">
-                <MenuLink href="/static/Joan Rieu - Resume.pdf">Resume (PDF)</MenuLink>
-                <MenuLink href="https://www.linkedin.com/in/joanrieu/">LinkedIn</MenuLink>
-                <MenuLink href="https://github.com/joanrieu">GitHub</MenuLink>
-                <MenuLink href="https://twitter.com/joanrieu">Twitter</MenuLink>
-            </ul>
-        </div>
-        <section>
-            <div className="hero">
-                <h1>Joan Rieu</h1>
-                <p>Software engineering<br/>& systems architecture</p>
-            </div>
-            <blockquote>I build complex software systems.</blockquote>
-        </section>
+        <Section />
+        <Links />
     </div>
 )
