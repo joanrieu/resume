539b1c697a6962119eb479bf7304a4d737aa041c
Joan Rieu <joan@joanrieu.com>
2021-01-09 14:16:06 -0500

    add landing screen design

--- src/pug.config.js
+++ src/pug.config.js
@@ -1,7 +1,10 @@
-const resume = require("./Joan_Rieu.resume.json");
-
 module.exports = {
   locals: {
-    resume,
+    links: [
+      ["Resume (PDF)", "#"],
+      ["LinkedIn", "https://www.linkedin.com/in/joanrieu/"],
+      ["GitHub", "http://github.com/joanrieu"],
+      ["Twitter", "https://twitter.com/joanrieu"],
+    ],
   },
 };
