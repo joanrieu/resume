348f63df9ffd52148cf4f0aaf81d0cd156c82573
Joan Rieu <joan@joanrieu.com>
2021-01-15 00:42:31 -0500

    add resume link

--- src/pug.config.js
+++ src/pug.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   locals: {
     links: [
-      ["Resume (PDF)", "#"],
+      ["Resume (PDF)", "/Joan Rieu - Resume.pdf"],
       ["LinkedIn", "https://www.linkedin.com/in/joanrieu/"],
       ["GitHub", "http://github.com/joanrieu"],
       ["Twitter", "https://twitter.com/joanrieu"],
